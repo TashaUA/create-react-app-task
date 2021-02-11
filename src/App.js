f7dcbba484ded91b641d7433cbdec9916538101e
Nataliia Blokhina <Nataliia_Blokhina@epam.com>
2021-02-11 19:29:13 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+import React from 'react';
+
+const header = React.createElement("h1", null, "My first reactJS app");
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {header}
+        <p>
+          Hello world.
+        </p>
+      </header>
+      <FilmsList/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
+class FilmsList extends React.Component {
+    render() {
+        const dataFilms = [
+            {
+                id: '1',
+                name: 'Inception',
+            },
+            {
+                id: '2',
+                name: 'Avengers',
+            },
+            {
+                id: '3',
+                name: 'Reservoir dogs',
+            },
+            {
+                id: '4',
+                name: 'Kill Bill',
+            },
+        ];
+        return (
+            <ul className="films-list">
+                {dataFilms.map((el) => (
+                    <Film key={el.id} name={el.name} />
+                ))}
+            </ul>
+        );
+    }
+}
+
+function Film({ id, name }) {
+    return (
+        <li className="films-list-item" key={id}>
+            {name}
+        </li>
+    );
+}
+
+class Footer extends React.PureComponent {
+
+    render() {
+        return <p className="footer">All right are reserved.</p>
+    }
+}
